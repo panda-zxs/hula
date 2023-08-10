0b2344abd3f5a0151813b33d5697163ffd755917
panda-zxs <895910599@qq.com>
2023-08-10 10:25:38 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
-
-git pull
-
+git checkout -- .
+git config user.email "bot@example.com"
+git config user.name "a bot"
+git config pull.rebase false
+git pull origin master
 cd $(dirname $0)
 
 # 执行次数
