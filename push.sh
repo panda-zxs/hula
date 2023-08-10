0ecf59b633cbde122ab09a7c843528a65340f454
panda-zxs <895910599@qq.com>
2023-08-10 10:28:08 +0800

    2

--- push.sh
+++ push.sh
@@ -3,6 +3,8 @@ git checkout -- .
 git config user.email "bot@example.com"
 git config user.name "a bot"
 git config pull.rebase false
+git remote add bot https://github.com/panda-zxs/hula.git
+git fetch
 git pull origin master
 cd $(dirname $0)
 
