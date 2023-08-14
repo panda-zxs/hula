0d76665973aa0eaf465540c22cca857eb03d04b7
panda-zxs <895910599@qq.com>
2023-08-14 10:06:23 +0800

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 git remote remove origin
-git remote add origin https://panda-zxs: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@github.com/panda-zxs/hula.git
+git remote add origin https://panda-zxs:$PASSWD@github.com/panda-zxs/hula.git
 git config user.email "45396622zxs@gmail.com"
 git config user.name "panda-zxs"
 git config pull.rebase false
