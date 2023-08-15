9456b0dafe8e9839874f4f2d008efe07e9e58fab
panda-zxs <45396622zxs@gmail.com>
2023-08-15 18:28:10 +0800

    feat：消息处理

---
+++ start.sh
@@ -0,0 +1,26 @@
+#! /bin/bash
+git remote remove origin
+git remote add origin git@github.com/panda-zxs/hula.git
+git config user.email "45396622zxs@gmail.com"
+git config user.name "panda-zxs"
+git config pull.rebase false
+git checkout -- .
+git fetch origin
+git pull origin master
+cd $(dirname $0)
+
+# 执行次数
+count=$(node ./commit/ran.js)
+echo $count
+
+# 循环执行
+for((i=0;i<$count;i++))
+do
+node ./commit/code.js
+git add . --all
+
+msg=$(node ./commit/msg.js)
+git commit -m "$msg"
+
+git push origin master
+done
